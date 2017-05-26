433a1365395ba7224a10b40b5a29da0f6f598ef7
Famodun Gbolaga <biyibg@yahoo.co.uk>
2017-05-26 21:13:29 +0300

    third draft

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	//'url' : 'mongodb://gbolaga:ayokunle@localhost:27017/catdb'
 	//'url' : `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/catdb`
-	//'url' : `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/catdb`
-	'url' : `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00-ahxal.mongodb.net:27017,cluster0-shard-00-01-ahxal.mongodb.net:27017,cluster0-shard-00-02-ahxal.mongodb.net:27017/${process.env.DB_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
-
+	'url' : `mongodb://heroku_p3hqssg8:immc6pkagejiate24raau4lloo@ds155651.mlab.com:55651/heroku_p3hqssg8`
+	//'url' : `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00-ahxal.mongodb.net:27017,cluster0-shard-00-01-ahxal.mongodb.net:27017,cluster0-shard-00-02-ahxal.mongodb.net:27017/${process.env.DB_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
 }

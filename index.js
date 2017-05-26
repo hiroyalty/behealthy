ed22f27fe4c43ff460a3f5d165087873282a249c
Famodun Gbolaga <biyibg@yahoo.co.uk>
2017-05-26 22:54:07 +0300

    testing chat

--- index.js
+++ index.js
@@ -83,10 +83,75 @@ if (app.get('env') === 'development') {
     });
 }
 
-//app.get('/', function (req, res) {
- //  res.send('Hello World!');
-//});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = server;
+ios.sockets.on('connection', (socket) => {
+	var query = Chat.find({ msgtype: 'publicchat' });
+	query.sort('-created').limit(5).exec(function(err, docs){
+		if(err) throw err;
+		console.log('docs ' + docs);
+		socket.emit('load old msgs', docs);
+	});	
+	
+	socket.on('new user', (data, callback) => {
+		if(data in users) {
+			callback(false);
+		} else {
+			callback(true);
+			socket.nickname = data;
+			users[socket.nickname] = socket;
+			updateNicknames(); //io.sockets.emit('usernames', nicknames);
+		}
+	});
+	
+	function updateNicknames(){
+		ios.sockets.emit('usernames', Object.keys(users));
+	}
+	
+	socket.on('send message', (data, callback) => {
+		var msg = data.trim();
+		if(msg.substr(0,3) == '/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if(ind !== -1) {
+				var name = msg.substr(0, ind);//check if username is valid
+				var msg = msg.substr(ind + 1);
+				if(name in users){
+					users[name].emit('whisper', {msg: msg, nick: socket.nickname}); //send the trim data instead 'msg' line 36 
+					console.log('whisper:');
+				} else {
+					callback('Error! Enter a valid user.');
+				}
+				
+			} else{
+				callback('Error: Please enter a message for your whisper.');
+			}
+		} else {
+			//var newMsg = new Chat({msg: msg, nick: socket.nickname, time: moment().format('YYYY-MM-DD HH:mm:ss')});
+			var newMsg = new Chat({msg: msg, nick: socket.nickname, time: moment().format('YYYY-MM-DD HH:mm:ss'), msgtype: 'publicchat' });
+			newMsg.save(function(err){
+				if(err) throw err;
+				ios.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+			});
+		//socket.broadcast.emit('new message', data); To everyone except the user.
+		}
+	});
+	
+	socket.on('whisperadmin', (data, callback) => {
+		var msg = data.trim();
+		var newMsg = new Chat({msg: msg, nick: socket.nickname, time: moment().format('YYYY-MM-DD HH:mm:ss'), msgtype: 'privatechat' });
+		newMsg.save(function(err){
+			if(err) throw err;
+			users['admin'].emit('newwhisperadmin', {msg: msg, nick: socket.nickname});
+		});
+	});
+	
+	socket.on('disconnect', (data) => {
+		if(!socket.nickname) return;
+		delete users[socket.nickname];
+		updateNicknames();
+	});
 });
